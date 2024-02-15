096458cda4765437926fabd050f4c479d3d6bf3e
Wid-Gaël Destin <16321487+wdestin@users.noreply.github.com>
2024-02-14 21:50:49 -0500

    feat(hero): add hero section

---
+++ layouts/HeroLayout.tsx
@@ -0,0 +1,15 @@
+export default function HeroLayout() {
+  return (
+    <>
+      <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+        <div className="text-left">
+          <h1 className="text-4xl font-bold sm:text-6xl">Wid-Gaël</h1>
+          <p className="mt-2 text-2xl leading-6">
+            Previously at Thales Digital Identity & Security as a Software Engineer.
+          </p>
+          <p className="mt-2 text-2xl leading-6">Based in Toronto, Canada.</p>
+        </div>
+      </div>
+    </>
+  )
+}

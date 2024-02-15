3440a22661e52f23c88ac3278670f2d63520b6af
Wid-Gaël Destin <16321487+wdestin@users.noreply.github.com>
2024-02-14 19:24:36 -0500

    chore(data): add contact links to header nav

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -2,6 +2,10 @@ const headerNavLinks = [
   { href: '/', title: 'Home' },
   { href: '#projects', title: 'Projects' },
   { href: '#blog', title: 'Blog' },
+  { href: 'mailto:hello@wdestin.xyz', title: 'Email' },
+  { href: 'https://x.com/widgael', title: 'X' },
+  { href: 'https://www.linkedin.com/in/wdestin', title: 'LinkedIn' },
+  { href: 'https://github.com/wdestin', title: 'GitHub' },
 ]
 
 export default headerNavLinks

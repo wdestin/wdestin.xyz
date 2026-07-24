4a8b75ef73ae31913079cda0afc02b830cd1d119
Wid-Gaël Destin <16321487+wdestin@users.noreply.github.com>
2026-07-23 21:33:09 -0400

    chore: clean up link hub tooling

--- next.config.js
+++ next.config.js
@@ -71,13 +71,5 @@ module.exports = () => {
         },
       ]
     },
-    webpack: (config) => {
-      config.module.rules.push({
-        test: /\.svg$/,
-        use: ['@svgr/webpack'],
-      })
-
-      return config
-    },
   })
 }

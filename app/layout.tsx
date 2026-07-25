6f5523a7e963c62e1bf5502326ddf18f9a0d61c1
Wid-Gaël Destin <16321487+wdestin@users.noreply.github.com>
2026-07-24 21:29:57 -0400

    chore: remove google font build dependency

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,9 @@
 import 'css/tailwind.css'
 
-import { Space_Grotesk } from 'next/font/google'
 import Script from 'next/script'
 import Footer from '@/components/Footer'
 import siteMetadata from '@/data/siteMetadata'
 import { Metadata } from 'next'
-
-const space_grotesk = Space_Grotesk({
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-space-grotesk',
-})
 
 export const metadata: Metadata = {
   metadataBase: new URL(siteMetadata.siteUrl),
@@ -51,7 +44,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang={siteMetadata.language} className={`${space_grotesk.variable} scroll-smooth`}>
+    <html lang={siteMetadata.language} className="scroll-smooth">
       <link rel="apple-touch-icon" sizes="76x76" href="/static/favicons/apple-touch-icon.png" />
       <link rel="icon" type="image/png" sizes="32x32" href="/static/favicons/favicon-32x32.png" />
       <link rel="icon" type="image/png" sizes="16x16" href="/static/favicons/favicon-16x16.png" />
